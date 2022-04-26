d211505590edd72d0baf9d5b953a79cf1b6fa70c
Hanwe Lee <mobiledev@kakao.com>
2022-04-27 00:06:33 +0900

    - 애플로그인 가입된 이메일인지 체크 로직 추가

--- Archive/Usecase/LoginOAuth/LoginWithAppleRepositoryImplement.swift
+++ Archive/Usecase/LoginOAuth/LoginWithAppleRepositoryImplement.swift
@@ -7,9 +7,12 @@
 
 import AuthenticationServices
 import RxSwift
+import SwiftyJSON
 
 class LoginWithAppleRepositoryImplement: NSObject {
     var completion: ((Result<String, ArchiveError>) -> Void)?
+    
+    private let disposeBag: DisposeBag = DisposeBag()
     
     func signIn(completion: @escaping (Result<String, ArchiveError>) -> Void) {
         self.completion = completion
@@ -27,10 +30,67 @@ class LoginWithAppleRepositoryImplement: NSObject {
     
     func isExistEmailWithApple(accessToken: String) -> Observable<Bool> {
         return Observable.create { [weak self] emitter in
-            emitter.onNext(false)
-            emitter.onCompleted()
-            // TODO: 작업
+            guard let email = self?.extractEmail(accessToken: accessToken) else {
+                emitter.onNext(false)
+                emitter.onCompleted()
+                return Disposables.create()
+            }
+            
+            let provider = ArchiveProvider.shared.provider
+            provider.rx.request(.isDuplicatedEmail(email))
+                .asObservable()
+                .subscribe(onNext: { [weak self] isDuplicatedEmailResponse in
+                    switch isDuplicatedEmailResponse.statusCode {
+                    case 200:
+                        if let resultJson: JSON = try? JSON.init(data: isDuplicatedEmailResponse.data) {
+                            let isDup = resultJson["duplicatedEmail"].boolValue
+                            if isDup {
+                                emitter.onNext(true)
+                                emitter.onCompleted()
+                            } else {
+                                emitter.onNext(false)
+                                emitter.onCompleted()
+                            }
+                        } else {
+                            emitter.onNext(false)
+                            emitter.onCompleted()
+                        }
+                    default:
+                        emitter.onNext(false)
+                        emitter.onCompleted()
+                    }
+                })
+                .disposed(by: self?.disposeBag ?? DisposeBag())
+            
             return Disposables.create()
+        }
+    }
+    
+    private func extractEmail(accessToken: String) -> String? {
+        var returnValue: String?
+        let splited = accessToken.split(separator: ".")
+        if splited.count != 3 {
+            return returnValue
+        }
+        var payloadBase64: String = String(splited[1])
+        if payloadBase64.hasSuffix("==") {
+        } else if payloadBase64.hasSuffix("=") {
+            payloadBase64 += "="
+        } else {
+            payloadBase64 += "=="
+        }
+        guard let decodedPayload = Data(base64Encoded: payloadBase64) else {
+            return returnValue
+        }
+        guard let payloadJson: JSON = try? JSON.init(data: decodedPayload) else {
+            return returnValue
+        }
+
+        returnValue = payloadJson["email"].stringValue
+        if returnValue == "" {
+            return nil
+        } else {
+            return returnValue
         }
     }
 }
