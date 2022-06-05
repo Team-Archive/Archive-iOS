e53d15313e65db411c401d469a1c9043c09ae183
Hanwe Lee <mobiledev@kakao.com>
2022-06-05 17:30:29 +0900

    누락 파일 추가

--- Archive/Define/EmotionDefine.swift
+++ Archive/Define/EmotionDefine.swift
@@ -11,6 +11,9 @@ enum Emotion: String, CaseIterable {
     case pleasant = "PLEASANT"
     case splendid = "BEAUTIFUL"
     case wonderful = "AMAZING"
+//    case interesting = "INTERESTING2" // TODO: 서버에서 어떻게 내려올까..?
+//    case fresh = "FRESH"
+//    case touching = "TOUCHING"
     
     static func fromString(_ str: String) -> Emotion? {
         for item in Emotion.allCases where item.rawValue == str {
