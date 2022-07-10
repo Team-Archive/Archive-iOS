47921053f0aff4dc4df5eec567e753b47a11911f
Hanwe Lee <mobiledev@kakao.com>
2022-07-10 23:16:00 +0900

    - 필터 뷰 컨트롤러 추가

--- Archive/Common/ViewController/FilterViewController.swift
+++ Archive/Common/ViewController/FilterViewController.swift
@@ -24,6 +24,13 @@ class FilterViewController: UIViewController {
         $0.backgroundColor = Gen.Colors.dim.color
     }
     
+    private lazy var dismissAreaView = UIView().then {
+        $0.backgroundColor = .clear
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissAction(_:)))
+        $0.isUserInteractionEnabled = true
+        $0.addGestureRecognizer(tap)
+    }
+    
     private let mainContentsView = UIView().then {
         $0.backgroundColor = .clear
     }
@@ -38,10 +45,12 @@ class FilterViewController: UIViewController {
         $0.text = "시간 순으로 보기"
     }
     
-    private let acceptBtn = UIButton().then {
+    private lazy var acceptBtn = UIButton().then {
         $0.setTitleAllState("적용")
         $0.setTitleColor(Gen.Colors.black.color, for: .normal)
         $0.setTitleColor(Gen.Colors.pressedBtn.color, for: .highlighted)
+        $0.titleLabel?.font = .fonts(.button)
+        $0.addTarget(self, action: #selector(confirm), for: .touchUpInside)
     }
     
     private let radioView = TimeFilterRadioButton().then {
@@ -54,11 +63,16 @@ class FilterViewController: UIViewController {
         $0.text = "감정 별로 보기"
     }
     
-    private let emotionSelectView = ArchiveSelectEmotionView().then {
+    private let emotionSelectView = ArchiveFilterEmotionView().then {
         $0.backgroundColor = .clear
     }
     
     // MARK: private property
+    
+    private let disposeBag = DisposeBag()
+    private var timeSortBy: TimeFilterRadioButton.sortType
+    private var emotionSortBy: Emotion?
+    
     
     // MARK: internal property
     
@@ -67,16 +81,23 @@ class FilterViewController: UIViewController {
     
     // MARK: lifeCycle
     
+    init(timeSortBy: TimeFilterRadioButton.sortType, emotionSortBy: Emotion?) {
+        self.timeSortBy = timeSortBy
+        self.emotionSortBy = emotionSortBy
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     deinit {
         print("\(self) deinit")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.modalPresentationStyle = .overFullScreen
-        let dimTap = UITapGestureRecognizer(target: self, action: #selector(self.dismissAction(_:)))
-        self.mainContentsView.isUserInteractionEnabled = true
-        self.mainContentsView.addGestureRecognizer(dimTap)
+        bind()
     }
     
     override func loadView() {
@@ -99,7 +120,7 @@ class FilterViewController: UIViewController {
                 $0.leading.equalTo(self.mainContentsView.snp.leading)
                 $0.trailing.equalTo(self.mainContentsView.snp.trailing)
                 $0.bottom.equalTo(self.mainContentsView.snp.bottom).offset(150)
-                $0.height.equalTo(200)
+                $0.height.equalTo(260)
             }
         case .fadeIn:
             self.bottomView.snp.makeConstraints {
@@ -108,16 +129,56 @@ class FilterViewController: UIViewController {
                 $0.bottom.equalTo(mainContentsView.snp.bottom)
             }
         }
-        bottomView.clipsToBounds = false
-        bottomView.layer.cornerRadius = 20
-        bottomView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner] // Top right corner, Top left corner respectively
+        self.bottomView.clipsToBounds = false
+        self.bottomView.layer.cornerRadius = 20
+        self.bottomView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
         self.bottomView.makeBottomCoverView()
+        
+        self.mainContentsView.addSubview(self.dismissAreaView)
+        self.dismissAreaView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(self.mainContentsView)
+            $0.bottom.equalTo(self.bottomView.snp.top)
+        }
         
         self.bottomView.addSubview(self.sortByTimeTitleLabel)
         self.sortByTimeTitleLabel.snp.makeConstraints {
             $0.top.equalTo(self.bottomView.snp.top).offset(28)
             $0.leading.equalTo(self.bottomView.snp.leading).offset(32)
         }
+        
+        self.bottomView.addSubview(self.acceptBtn)
+        self.acceptBtn.snp.makeConstraints {
+            $0.trailing.equalTo(self.bottomView.snp.trailing).offset(-32)
+            $0.centerY.equalTo(self.sortByTimeTitleLabel)
+        }
+        
+        self.bottomView.addSubview(self.radioView)
+        self.radioView.snp.makeConstraints {
+            $0.top.equalTo(self.sortByTimeTitleLabel.snp.bottom).offset(26)
+            $0.leading.equalTo(self.bottomView.snp.leading).offset(32)
+            $0.trailing.equalTo(self.bottomView.snp.trailing).offset(32)
+            $0.height.equalTo(24)
+        }
+        self.radioView.delegate = self
+        
+        self.bottomView.addSubview(self.sortByEmotionTitleLabel)
+        self.sortByEmotionTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.radioView.snp.bottom).offset(32)
+            $0.leading.trailing.equalTo(self.bottomView).offset(32)
+        }
+        
+        
+        self.bottomView.addSubview(self.emotionSelectView)
+        self.emotionSelectView.snp.makeConstraints {
+            $0.top.equalTo(self.sortByEmotionTitleLabel.snp.bottom).offset(26)
+            $0.leading.trailing.equalTo(self.bottomView)
+            $0.height.equalTo(77)
+        }
+        self.emotionSelectView.delegate = self
+    }
+    
+    private func bind() {
+        
     }
     
     // MARK: private function
@@ -146,6 +207,10 @@ class FilterViewController: UIViewController {
         }
     }
     
+    @objc private func confirm() {
+        print("얍")
+    }
+    
     // MARK: internal function
     
     func showEffect() {
@@ -158,6 +223,18 @@ class FilterViewController: UIViewController {
         }
     }
     
+}
 
+
+extension FilterViewController: TimeFilterRadioButtonDelegate, ArchiveFilterEmotionViewDelegate {
+    
+    func selectedSortBy(view: TimeFilterRadioButton, type: TimeFilterRadioButton.sortType) {
+        self.timeSortBy = type
+    }
+    
+    
+    func didSelectedItem(view: ArchiveSelectEmotionView, didSelectedAt index: Int) {
+        
+    }
     
 }
