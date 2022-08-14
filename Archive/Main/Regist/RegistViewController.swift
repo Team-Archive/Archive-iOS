458a543508056629b151ad12995f685865d55b7d
Hanwe Lee <mobiledev@kakao.com>
2022-08-15 00:39:53 +0900

    기본 등록 화면 작업

--- Archive/Main/Regist/RegistViewController.swift
+++ Archive/Main/Regist/RegistViewController.swift
@@ -43,43 +43,16 @@ class RegistViewController: UIViewController, View {
         $0.image = Gen.Images.navigationGradation.image
     }
     
-    private let foregroundBottomContentsView = UIView().then {
+    private let foregroundBottomContentsView = RegistForegroundBottomView().then {
         $0.backgroundColor = .clear
-    }
-     
-    private let foregroundBottomHelpTitleLabel = UILabel().then {
-        $0.font = .fonts(.subTitle)
-        $0.textColor = Gen.Colors.black.color
-        $0.text = "무슨 전시를 감상했나요?"
-    }
-    
-    private let foregroundBottomTitlePlaceHolderLabel = UILabel().then {
-        $0.font = .fonts(.header2)
-        $0.textColor = Gen.Colors.gray02.color
-        $0.text = "전시명을 입력해주세요."
-    }
-    
-    private let foregroundBottomTitleLabel = UILabel().then {
-        $0.font = .fonts(.header2)
-        $0.textColor = Gen.Colors.black.color
-    }
-    
-    private let foregroundBottomUnderLineView = UIView().then {
-        $0.backgroundColor = Gen.Colors.gray04.color
     }
     
     private let foregroundTopContentsView = UIView().then {
         $0.backgroundColor = .clear
     }
     
-    // step1
-    
-    private let foregroundTopStep1View = UIView().then {
+    private let foregroundTopStep1View = ForegroundStep1TopView().then {
         $0.backgroundColor = .clear
-    }
-    
-    private let foregroundTopPlaceHolderImgView = UIImageView().then {
-        $0.image = Gen.Images.emptyTicket.image
     }
     
     
@@ -145,7 +118,6 @@ class RegistViewController: UIViewController, View {
         self.foregroundContentsView.snp.makeConstraints {
             $0.edges.equalTo(self.foregroundScrollView).priority(750)
             $0.width.equalTo(self.foregroundScrollView).priority(1000)
-            $0.height.equalTo(1500)
         }
         
         let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
@@ -156,53 +128,24 @@ class RegistViewController: UIViewController, View {
             $0.height.equalTo(statusBarHeight + (self.navigationController?.navigationBar.bounds.height ?? 0))
         }
         
-//        private let  = UIView().then {
-//            $0.backgroundColor = .clear
-//        }
-//
-//        private lazy var topGradationView = UIImageView().then {
-//            $0.backgroundColor = .clear
-//            $0.image = Gen.Images.navigationGradation.image
-//        }
-//
-//        private let foregroundBottomContentsView = UIView().then {
-//            $0.backgroundColor = .clear
-//        }
-//
-//        private let foregroundBottomHelpTitleLabel = UILabel().then {
-//            $0.font = .fonts(.subTitle)
-//            $0.textColor = Gen.Colors.black.color
-//            $0.text = "무슨 전시를 감상했나요?"
-//        }
-//
-//        private let foregroundBottomTitlePlaceHolderLabel = UILabel().then {
-//            $0.font = .fonts(.header2)
-//            $0.textColor = Gen.Colors.gray02.color
-//            $0.text = "전시명을 입력해주세요."
-//        }
-//
-//        private let foregroundBottomTitleLabel = UILabel().then {
-//            $0.font = .fonts(.header2)
-//            $0.textColor = Gen.Colors.black.color
-//        }
-//
-//        private let foregroundBottomUnderLineView = UIView().then {
-//            $0.backgroundColor = Gen.Colors.gray04.color
-//        }
-//
-//        private let foregroundTopContentsView = UIView().then {
-//            $0.backgroundColor = .clear
-//        }
-//
-//        // step1
-//
-//        private let foregroundTopStep1View = UIView().then {
-//            $0.backgroundColor = .clear
-//        }
-//
-//        private let foregroundTopPlaceHolderImgView = UIImageView().then {
-//            $0.image = Gen.Images.emptyTicket.image
-//        }
+        self.foregroundContentsView.addSubview(self.foregroundBottomContentsView)
+        self.foregroundBottomContentsView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalTo(self.foregroundContentsView)
+            $0.height.equalTo(170)
+        }
+        
+        self.foregroundContentsView.addSubview(self.foregroundTopContentsView)
+        self.foregroundTopContentsView.snp.makeConstraints {
+            $0.leading.trailing.top.equalTo(self.foregroundContentsView)
+            $0.height.equalTo(UIScreen.main.bounds.width * 1.621333333)
+            $0.bottom.equalTo(self.foregroundBottomContentsView.snp.top)
+        }
+        
+        self.foregroundTopContentsView.addSubview(self.foregroundTopStep1View)
+        self.foregroundTopStep1View.snp.makeConstraints {
+            $0.edges.equalTo(self.foregroundTopContentsView)
+        }
+        
     }
     
     override func viewDidLoad() {
