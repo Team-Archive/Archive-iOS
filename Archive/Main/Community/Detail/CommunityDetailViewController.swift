c8f94040d91cd698d5ae55a6aed223277d2c40d4
Hanwe Lee <mobiledev@kakao.com>
2022-10-25 15:05:09 +0900

    디테일 데이터에 인덱스 추가

--- Archive/Main/Community/Detail/CommunityDetailViewController.swift
+++ Archive/Main/Community/Detail/CommunityDetailViewController.swift
@@ -160,6 +160,8 @@ class CommunityDetailViewController: UIViewController, View, ActivityIndicatorab
     
     // MARK: private property
     
+    private var currentIndex: Int = -1
+    
     // MARK: property
     var disposeBag: DisposeBag = DisposeBag()
     
@@ -374,9 +376,10 @@ class CommunityDetailViewController: UIViewController, View, ActivityIndicatorab
         reactor.state
             .map { $0.detailArchive }
             .distinctUntilChanged()
-            .asDriver(onErrorJustReturn: .init(archiveInfo: .init(archiveId: 0, authorId: 0, name: "", watchedOn: "", emotion: .fun, companions: nil, mainImage: "", images: nil), innerIndex: 0))
+            .asDriver(onErrorJustReturn: .init(archiveInfo: .init(archiveId: 0, authorId: 0, name: "", watchedOn: "", emotion: .fun, companions: nil, mainImage: "", images: nil), innerIndex: 0, index: 0))
             .drive(onNext: { [weak self] data in
                 if data.innerIndex == 0 {
+                    print("....")
                     self?.showCover(infoData: data)
                 } else {
                     self?.showPhoto(infoData: data)
@@ -398,7 +401,7 @@ class CommunityDetailViewController: UIViewController, View, ActivityIndicatorab
         reactor.state
             .map { $0.detailArchive }
             .distinctUntilChanged()
-            .asDriver(onErrorJustReturn: .init(archiveInfo: .init(archiveId: 0, authorId: 0, name: "", watchedOn: "", emotion: .fun, companions: nil, mainImage: "", images: nil), innerIndex: 0))
+            .asDriver(onErrorJustReturn: .init(archiveInfo: .init(archiveId: 0, authorId: 0, name: "", watchedOn: "", emotion: .fun, companions: nil, mainImage: "", images: nil), innerIndex: 0, index: 0))
             .drive(onNext: { [weak self] data in
                 self?.likeBtn.isLike = LikeManager.shared.likeList.contains("\(data.archiveInfo.archiveId)")
             })
@@ -448,6 +451,7 @@ class CommunityDetailViewController: UIViewController, View, ActivityIndicatorab
     
     private func showCover(infoData: CommunityReactor.DetailInfo) {
         print("info: \(infoData.innerIndex)")
+        print("info2: \(infoData.index)")
         let item = infoData.archiveInfo
         self.topCoverContentsView.isHidden = false
         self.bottomCoverContentsView.isHidden = false
@@ -469,6 +473,15 @@ class CommunityDetailViewController: UIViewController, View, ActivityIndicatorab
 //            $0.font = .fonts(.body)
 //            $0.textColor = Gen.Colors.gray03.color
 //        }
+        
+        if self.currentIndex != -1 {
+            if currentIndex > infoData.index {
+                print("이전 유저 데이터보기")
+            } else if currentIndex < infoData.index {
+                print("다음 유저 데이터보기")
+            }
+        }
+        self.currentIndex = infoData.index // 위치가 별로긴 한데.. 애니메이션때문에 우선 여기에 넣고 개선해보자.. showCover 라는 메서드에서 바꿔주는걸 파악하고있지 않으면 알 수가 없음...
         
     }
     
