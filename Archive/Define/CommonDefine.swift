bda1cdc7535227c202e0dc3f58c164562d7616b7
Hanwe Lee <mobiledev@kakao.com>
2022-11-09 21:23:15 +0900

    개인정보, 약관 URL 변경

--- Archive/Define/CommonDefine.swift
+++ Archive/Define/CommonDefine.swift
@@ -18,6 +18,9 @@ class CommonDefine: NSObject {
     static let kakaoAppKey: String = "147a5c186ee0f5fdc58244b704165132"
     static let devApiServer: String = "https://archive-ticket.site/dev" // 개발
     static let passwordForDebugCheatKey: String = "goLang"
+    static let privacyUrl = "https://www.notion.so/0370a9320f2d4c2286db8d276d378712"
+    static let termsUrl = "https://www.notion.so/f6b1cf943c074182a69fe8433df2eca0"
+    static let aboutArchiveUrl = "https://www.notion.so/Preview-d2a89a612a694ec8a8fc180c8efb9c39"
 #if DEBUG
     static let apiServer: String = "https://archive-ticket.site/dev" // 개발
 //    static let apiServer: String = "https://archive-ticket.site/prd" // 실서버
--- Archive/Onboarding/SignUp/SignUpReactor.swift
+++ Archive/Onboarding/SignUp/SignUpReactor.swift
@@ -131,14 +131,14 @@ final class SignUpReactor: Reactor, Stepper {
             return .just(.setTermsAgreement(isSelected))
             
         case .viewTerms:
-            guard let url = URL(string: "https://wise-icicle-d10.notion.site/8ad4c5884b814ff6a6330f1a6143c1e6") else { return .empty()}
+            guard let url = URL(string: CommonDefine.termsUrl) else { return .empty()}
             self.steps.accept(ArchiveStep.openUrlIsRequired(url: url, title: "이용약관"))
             return .empty()
         case .agreePersonalInformationPolicy:
             let isSelected = !currentState.isAgreePersonalInformationPolicy
             return .just(.setPersonalInformationPolicyAgreement(isSelected))
         case .viewPersonalInformationPolicy:
-            guard let url = URL(string: "https://wise-icicle-d10.notion.site/13ff403ad4e2402ca657fb20be31e4ae") else { return .empty()}
+            guard let url = URL(string: CommonDefine.privacyUrl) else { return .empty()}
             self.steps.accept(ArchiveStep.openUrlIsRequired(url: url, title: "개인정보 처리방침"))
             return .empty()
         case .completeAgreePolicy:
